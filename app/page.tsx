bd9158383ab29e021ad388a0f72f1312201091f8
hermescati <hermescati99@gmail.com>
2023-12-07 11:42:46 +0100

    Created button component with variants

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import Button from "@/components/Button/Button";
-import "tailwindcss/tailwind.css";
+"use client";
 
 export default function Home() {
   return (
     <section>
-      <Button intent="primary" content="Button label" />
-      <Button intent="secondary" content="Button label" />
+      <p>Main Page</p>
     </section>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,11 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    variants: {
+      extend: {
+        opacity: ["disabled"],
+      },
+    },
     colors: {
       transparent: "transparent",
       black: "#000",
