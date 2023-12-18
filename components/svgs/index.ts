aebdb817c281ccc7eac9af914462dd6aa8251cdb
hermescati <hermescati99@gmail.com>
2023-12-19 00:18:03 +0100

    Resolved issues

--- components/svgs/index.ts
+++ components/svgs/index.ts
@@ -1,4 +1,12 @@
-declare module "@/components/SVGs/*.svg" {
+declare module "@/components/SVGs/brands/*.svg" {
+  import React from "react";
+
+  const Component: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+
+  export default Component;
+}
+
+declare module "@/components/SVGs/icons/*.svg" {
   import React from "react";
 
   const Component: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
